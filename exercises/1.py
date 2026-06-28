299c0611b1f6a93afc9a561884b0dfaaa15cf1d2
Akshat-Bhandari <bhandariakshat08@gmail.com>
2026-06-28 11:12:17 +0530

    Operators and Data Types

--- exercises/1.py
+++ exercises/1.py
@@ -6,12 +6,14 @@ print(3/4)
 print(3**4)
 print(3//4)
 print("\n")
+
 #Strings
 print("Akshat")
 print("Bhandari")
 print("India")
 print("I am enjoying learning Python")
 print("\n")
+
 #Data Types
 print(type(10))
 print(type(9.8))
@@ -21,3 +23,24 @@ print(type(['Asabeneh', 'Python', 'Finland']))
 print(type('Akshat'))
 print(type('Bhandari'))
 print(type('India'))
+print("\n")
+
+#Data Type examples
+print("Data Types")
+print("Numbers:")
+print("  Integer:",10)
+print("  Float:",2.2)
+print("  Complex:",1+1j)
+print("String:","Akshat")
+print("Boolean:",True)
+print("List:",['a','b','c'])
+print("Tuple:",('a','b','c'))
+print("Set:",{'a','b','c'})
+print("Dictionary:",{'Name':'Akshat','Surname':'Bhandari'})
+print("\n")
+
+#Find an Euclidean distance between (2, 3) and (10, 8)
+x1,y1 = 2,3
+x2,y2 = 10,8
+distance =((x2-x1)**2 + (y2-y1)**2)**0.5
+print("Euclidean distance between (2, 3) and (10, 8) is:",distance)
