1624a708f9c377381008cd2bed969d9c8333ef11
Akshat-Bhandari <bhandariakshat08@gmail.com>
2026-06-30 19:24:01 +0530

    Variables and Built-in Funtions

---
+++ exercises/2.py
@@ -0,0 +1,12 @@
+#2
+
+#Declaring Variables
+first_name = "Akshat"
+last_name = "Bhandari"
+full_name ="Akshat Bhandari"
+country = "India"
+city ="Dehradun"
+age = 18
+year = 2026
+lane,street,city,state= 22,"Turner Road","Dehradun","Uttarakhand"
+
