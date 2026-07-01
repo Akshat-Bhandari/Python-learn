7d1742940f55f40331c2b8ca9d91ea6d33d6739c
Akshat-Bhandari <bhandariakshat08@gmail.com>
2026-07-01 12:54:01 +0530

    Operators

---
+++ exercises/3.py
@@ -0,0 +1,29 @@
+age=18
+height=5.10
+complex=1+6j
+
+base=float(input("Enter the length of the base of a triangle: "))
+height=float(input("Enter the height of the triangle: "))
+area = 0.5*base*height
+print("The area of the triangle is:", area)
+
+a=int(input("Enter side a of a triangle: "))
+b=int(input("Enter side b of a triangle: "))
+c=int(input("Enter side c of a triangle: "))
+Perimeter = a+b+c
+print("The perimeter of the triangle is:", Perimeter)
+
+length=float(input("Enter the length of a rectangle: "))
+breadth=float(input("Enter the breadth of a rectangle: "))
+area_of_rectangle=length*breadth
+print("The area of the rectangle is:", area_of_rectangle)
+perimeter_of_rectangle=2*(length+breadth)
+print("The perimeter of the rectangle is:", perimeter_of_rectangle)
+
+pi=3.14
+radius = float(input("Enter the radius of the circle: "))
+area_of_circle = pi*(radius**2)
+print("The area of the circle is:", area_of_circle)
+circum_of_circle = 2*pi*radius
+print("The circumference of the circle is:", circum_of_circle)
+
