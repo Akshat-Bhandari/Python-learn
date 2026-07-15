81b52bb076c477150769103242bc5cad7385a3ca
Akshat-Bhandari <bhandariakshat08@gmail.com>
2026-07-15 23:02:18 +0530

    Sets

---
+++ exercises/7.py
@@ -0,0 +1,24 @@
+# sets
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+print(len(it_companies))
+it_companies.add('Twitter')
+it_companies.update(['Meta','Nvidia'])
+it_companies.remove("Oracle")
+
+print(A|B) #or we can write print(A.union(B))
+print(A&B) #or we can write print(A.intersection(B))
+print(A.issubset(B))
+print(A.isdisjoint(B))
+print(A.update(B))
+print(B.union(A))
+print(A.symmetric_difference(B))
+del A
+del B
+
+print(len(age))
+AGE=list(age)
+print(len(AGE))
