bb8e0db1082b9fab32f12b979b20393bfd2ea243
Akshat-Bhandari <bhandariakshat08@gmail.com>
2026-07-23 23:56:30 +0530

    Loops

---
+++ exercises/10.py
@@ -0,0 +1,31 @@
+i=0
+for x in range(0,11):
+    print(i)
+    i=i+1
+i=0
+print("\n")
+
+while i<=10:
+    print(i)
+    i+=1
+print("\n")
+
+j=10
+for x in range(0,11):
+    print(j)
+    j=j-1
+j=10
+print("\n")
+while j>=0:
+    print(j)
+    j=j-1
+print("\n")
+i=1
+for x in range(7):
+    print('#'*i)
+    i=i+1
+print("\n")
+
+for j in range(7):
+        print('# '*7)
+
